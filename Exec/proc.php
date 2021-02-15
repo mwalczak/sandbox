6e3abac649592d5149b19e3a83ba72611c98872b
m.walczak <m.walczak@inis.pl>
2021-02-15 13:25:16 +0100

    exec test

---
+++ Exec/proc.php
@@ -0,0 +1,3 @@
+<?php
+sleep(1);
+echo date('Y-m-d H:i:s').PHP_EOL;
---
+++ Exec/test-background.php
@@ -0,0 +1,11 @@
+<?php
+$log = '/tmp/log';
+$cmd = sprintf('php %s >> %s &', __DIR__.'/proc.php', $log);
+for ($i = 0; $i < 5; $i++) {
+    echo date('Y-m-d H:i:s') . PHP_EOL;
+    echo $cmd . PHP_EOL;
+    exec($cmd);
+}
+$logs = file_get_contents($log);
+echo 'Log:' . PHP_EOL;
+echo $logs . PHP_EOL;
