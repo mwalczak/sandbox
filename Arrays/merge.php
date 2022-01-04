99989957bf7123b0f6b65cf65ee8aa9986aaad81
mwalczak <m.walczak@inis.pl>
2022-01-04 09:03:27 +0100

    array merge perf tests

---
+++ Arrays/merge.php
@@ -0,0 +1,12 @@
+<?php
+global $array1, $array2;
+require_once __DIR__ . '/pre.php';
+
+$start_time=microtime(true);
+
+$arraySum = array_merge($array1, $array2);
+
+echo "array_merge(array1, array2)\ntime: ". (microtime(true) - $start_time). "\n";
+echo "memory (byte): ". memory_get_peak_usage(true). "\n\n";
+
+
